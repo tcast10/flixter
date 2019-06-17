c0067427b47441a454b7b13cb7f118b30b980fff
Teddy <tcastro@sandiego.edu>
2019-06-17 19:49:55 +0000

    Flixter 28 Try1

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,23 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enroll_for_current_course, only: [:show]
+
   def show
+    @lesson = current_lesson.section.course.show(current_lesson)
+    if current_user.enrolled_in?
+      redirect_to lesson_path(@course)
+    else
+      redirect_to current_course
+    end
   end
 
   private
+
+  def require_enroll_for_current_course
+    if current_user.enrolled_in? != current_user
+      redirect_to current_course, alert: 'You Need to Enroll First'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
