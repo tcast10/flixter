1cd497a062adfb068da038d0932cb70feb20438a
Teddy <tcastro@sandiego.edu>
2019-06-26 21:53:03 +0000

    Flixter Fin3

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -31,6 +31,12 @@ class Instructor::SectionsController < ApplicationController
     end
   end
 
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
   helper_method :current_section
   def current_section
     @current_section ||= Section.find(params[:id])
